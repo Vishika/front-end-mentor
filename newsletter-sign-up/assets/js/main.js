389f519ea62eabac57516785ff39a5c19f8edae9
vish <jalitha.vithanage@gmail.com>
2024-07-26 13:25:32 +0800

    newsletter sign up - validation javascript

---
+++ newsletter-sign-up/assets/js/main.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const successScreen = document.getElementById("success");
+const successBtn = document.getElementById("btn--success");
+const signupForm = document.getElementById("signup-form");
+const emailError = document.getElementById("email-error");
+const emailInput = document.getElementById("email");
+const emailConfirmation = document.getElementById("email-confirmation");
+
+const signup = function (event) {
+  event.preventDefault();
+  emailError.textContent = "";
+  emailInput.classList.remove("error");
+  const { email } = Object.fromEntries(new FormData(event.target));
+  if (emailInput.value && emailInput.validity.valid) {
+    emailConfirmation.textContent = emailInput.value;
+    successScreen.classList.toggle("hidden");
+    emailInput.value = "";
+  } else {
+    emailError.textContent = "Valid email required";
+    emailInput.classList.add("error");
+  }
+};
+
+const dismiss = function (event) {
+  event.preventDefault();
+  successScreen.classList.toggle("hidden");
+  emailConfirmation.textContent = "";
+};
+
+signupForm.addEventListener("submit", signup);
+successBtn.addEventListener("click", dismiss);
