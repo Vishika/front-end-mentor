285419061790090d92f7200e7f57e45697840afd
vish <jalitha.vithanage@gmail.com>
2024-07-29 13:41:16 +0800

    newsletter sign up - desktop

--- newsletter-sign-up/assets/js/main.js
+++ newsletter-sign-up/assets/js/main.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const successScreen = document.getElementById("success");
+const signupScreen = document.getElementById("signup");
 const successBtn = document.getElementById("btn--success");
 const signupForm = document.getElementById("signup-form");
 const emailError = document.getElementById("email-error");
@@ -14,6 +15,7 @@ const signup = function (event) {
   const { email } = Object.fromEntries(new FormData(event.target));
   if (emailInput.value && emailInput.validity.valid) {
     emailConfirmation.textContent = emailInput.value;
+    signupScreen.classList.toggle("hidden");
     successScreen.classList.toggle("hidden");
     emailInput.value = "";
   } else {
@@ -25,6 +27,7 @@ const signup = function (event) {
 const dismiss = function (event) {
   event.preventDefault();
   successScreen.classList.toggle("hidden");
+  signupScreen.classList.toggle("hidden");
   emailConfirmation.textContent = "";
 };
 
