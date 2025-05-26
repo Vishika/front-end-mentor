f320279221d7ef7e9718b1bbfa124be7e23534b1
Vishika <jalitha.vithanage@gmail.com>
2025-05-26 13:59:54 +0800

    ecommerce product page - mobile cart

--- ecommerce-product-page/script.js
+++ ecommerce-product-page/script.js
@@ -1,14 +1,30 @@
 "use strict";
 
+let currentAmount = 0;
+let cartAmount = 0;
+const productPrice = 125;
+
 const nav = document.getElementById("nav");
 const navOpen = document.getElementById("nav-open");
 const navClose = document.getElementById("nav-close");
-const cartOpen = document.getElementById("cart-open");
+const cartOpenBtn = document.getElementById("cart-open");
 const profileBtn = document.getElementById("profile-btn");
 const media = window.matchMedia("(min-width: 69.375em)");
 const main = document.getElementById("main");
 const slidePrev = document.getElementById("slide-prev");
 const slideNext = document.getElementById("slide-next");
+const decrementAmountBtn = document.getElementById("decrement-amount");
+const incrementAmountBtn = document.getElementById("increment-amount");
+const amountOutput = document.getElementById("amount");
+const addToCartBtn = document.getElementById("add-to-cart-btn");
+const cart = document.getElementById("cart");
+const cartEmptySection = document.getElementById("cart-empty");
+const cartNotEmptySection = document.getElementById("cart-not-empty");
+const cartProductPriceOutput = document.getElementById("cart-product-price");
+const cartAmountOutput = document.getElementById("cart-amount");
+const cartTotalOutput = document.getElementById("cart-total");
+const cartDeleteBtn = document.getElementById("cart-delete");
+const cartAmountBubble = document.getElementById("cart-amount-bubble");
 
 function setupNav(event) {
   if (media.matches) {
@@ -24,14 +40,14 @@ const openNav = function () {
   navClose.focus();
   nav.removeAttribute("inert");
   main.setAttribute("inert", "");
-  cartOpen.setAttribute("inert", "");
+  cartOpenBtn.setAttribute("inert", "");
   profileBtn.setAttribute("inert", "");
 };
 
 const closeNav = function () {
   navOpen.setAttribute("aria-expanded", false);
   main.removeAttribute("inert");
-  cartOpen.removeAttribute("inert");
+  cartOpenBtn.removeAttribute("inert");
   profileBtn.removeAttribute("inert");
   if (!media.matches) {
     nav.setAttribute("inert", "");
@@ -87,6 +103,44 @@ const nextSlide = function () {
   slides[next].classList.add("active");
 };
 
+const decrementAmount = function () {
+  if (currentAmount > 0) amountOutput.textContent = --currentAmount;
+};
+
+const incrementAmount = function () {
+  if (currentAmount < 99) amountOutput.textContent = ++currentAmount;
+};
+
+const updateCart = function () {
+  if (cartAmount > 0) {
+    cartEmptySection.classList.remove("active");
+    cartNotEmptySection.classList.add("active");
+    cartAmountBubble.classList.add("active");
+  } else {
+    cartEmptySection.classList.add("active");
+    cartNotEmptySection.classList.remove("active");
+    cartAmountBubble.classList.remove("active");
+  }
+  cartAmountBubble.textContent = cartAmount;
+  cartProductPriceOutput.textContent = "$" + productPrice + ".00";
+  cartAmountOutput.textContent = cartAmount;
+  cartTotalOutput.textContent = "$" + cartAmount * productPrice + ".00";
+};
+
+const addToCart = function () {
+  cartAmount += currentAmount;
+  updateCart();
+};
+
+const toggleCart = function () {
+  cart.classList.toggle("active");
+};
+
+const deleteCart = function () {
+  cartAmount = 0;
+  updateCart();
+};
+
 navOpen.addEventListener("click", openNav);
 navClose.addEventListener("click", closeNav);
 nav.addEventListener("click", navLinkClicked);
@@ -94,5 +148,10 @@ nav.addEventListener("keyup", navLinkPressed);
 media.addEventListener("change", setupNav);
 slidePrev.addEventListener("click", prevSlide);
 slideNext.addEventListener("click", nextSlide);
+decrementAmountBtn.addEventListener("click", decrementAmount);
+incrementAmountBtn.addEventListener("click", incrementAmount);
+addToCartBtn.addEventListener("click", addToCart);
+cartOpenBtn.addEventListener("click", toggleCart);
+cartDeleteBtn.addEventListener("click", deleteCart);
 
 setupNav();
